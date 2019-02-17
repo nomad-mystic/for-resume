66b0bcb04899cfb1ca0b518c604736163d4bbad3
nomad-mystic <nomadmystics@gmail.com>
2019-02-16 17:11:16 -0800

    PI-32: Worked out a basic API call to make sure everything was working fine, added a couple util files for easy reterval, and fixed issue in the config for from in that was reloading page over and over.

--- resources/assets/scripts/routes/home.js
+++ resources/assets/scripts/routes/home.js
@@ -1,6 +1,9 @@
 export default {
   init() {
     // JavaScript to be fired on the home page
+    // Testing this works for home page
+    // const body = window.document.getElementsByTagName('BODY')[0];
+    // body.style.backgroundColor = '#000000';
   },
   finalize() {
     // JavaScript to be fired on the home page, after the init JS
