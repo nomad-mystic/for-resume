49ca76e8cc5e4c4a2f3bcd2922046339ba775f93
nomad-mystic <nomadmystics@gmail.com>
2019-04-08 00:27:17 -0700

    PI-76: Worked on the 404 page some it looks better in all screen sizes. Need to add an image and create the 500 page.

--- resources/views/404.blade.php
+++ resources/views/404.blade.php
@@ -1,12 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
+  <section>
+    <article>
+      <main>
+        <div class="_404">
+          @include('partials.page-header')
+          @if (!have_posts())
 
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
-    </div>
-    {!! get_search_form(false) !!}
-  @endif
+            <div class="alert alert-warning _404__sub-title">
+              {{ __('Sorry, but the page you were trying to view does not exist.', 'passion') }}
+            </div>
+            <div class="_404__search-form">
+              {!! get_search_form(false) !!}
+            </div>
+          @endif
+        </div>
+      </main>
+    </article>
+  </section>
 @endsection
