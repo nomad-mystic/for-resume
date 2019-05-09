ec646b8defb3815f776d62b31e337369280829de
nomad-mystic <nomadmystics@gmail.com>
2019-05-09 00:09:39 -0700

    PI-36: Worked out most of the laptop styles and based on the design spec.

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,31 +1,31 @@
 <footer class="footer">
-  <div class="footer__navigation">
-    @php(dynamic_sidebar('footer-navigation'))
-  </div>
-  <div class="footer__about-pi">
-    @php(dynamic_sidebar('footer-about-pi'))
-  </div>
-  <div class="footer__contact">
-    <h3>Contact</h3>
-    <div class="contact-details">
-      <?php
-      ob_start();
-      dynamic_sidebar("footer-contact");
-      $sidebar_output = ob_get_clean();
-      if (class_exists('App\PassionPlugins\CustomWidgets\PassionCustomWidgetContact')) {
-        echo apply_filters('my_sidebar_output', $sidebar_output);
-      }
-      ?>
+  <div class="footer__container">
+    <div class="footer__navigation">
+      @php(dynamic_sidebar('footer-navigation'))
     </div>
-    <div class="social-media">
+    <div class="footer__about-pi">
+      @php(dynamic_sidebar('footer-about-pi'))
+    </div>
+    <div class="footer__contact">
+      <h3>Contact</h3>
+      <div class="contact-details">
+        <?php
+        ob_start();
+        dynamic_sidebar("footer-contact");
+        $sidebar_output = ob_get_clean();
+        if (class_exists('App\PassionPlugins\CustomWidgets\PassionCustomWidgetContact')) {
+          echo apply_filters('my_sidebar_output', $sidebar_output);
+        }
+        ?>
+      </div>
       @include('partials.components.social-media')
     </div>
-  </div>
-  <div class="footer__newsletter">
-    @include('partials.components.newsletter')
-  </div>
-  <div class="footer__copyright">
-    <p>&copy;2019 - Passion Impact is a registered 501(C)3 Public Charity. EIN #: 46-5118525.</p>
-    <p>A proud member of the AmeriCorps National Service Network.</p>
+    <div class="footer__newsletter">
+      @include('partials.components.newsletter')
+    </div>
+    <div class="footer__copyright">
+      <p>&copy;2019 - Passion Impact is a registered 501(C)3 Public Charity. EIN #: 46-5118525.</p>
+      <p>A proud member of the AmeriCorps National Service Network.</p>
+    </div>
   </div>
 </footer>
