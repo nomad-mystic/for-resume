8cb2480db7879f367b7f0736a68ef227ac5ebc61
nomad-mystic <nomadmystics@gmail.com>
2019-05-20 23:15:46 -0700

    PI-58: Exposed data and created the template got get involved module.

--- app/controllers/front-page.php
+++ app/controllers/front-page.php
@@ -26,4 +26,20 @@ class FrontPage extends Controller
 
         return [];
     }
+
+    /**
+     * @author Keith Murphy | nomadmystics@gmail.com
+     * @description Get option values from Get Involved Settings options admin page
+     * @return array
+     */
+    public function get_get_involved_options (): array
+    {
+        if (class_exists('App\PassionPlugins\HomeSettings\PassionGetInvolvedHomeSettings')) {
+            $values = get_option('passion_get_involved_options');
+            var_dump($values);
+            return $values;
+        }
+
+        return [];
+    }
 }
--- resources/views/front-page.blade.php
+++ resources/views/front-page.blade.php
@@ -3,5 +3,6 @@
 @section('content')
   @while(have_posts()) @php(the_post())
     @include('partials.pages.front-page.create-impact')
+    @include('partials.pages.front-page.get-involved')
   @endwhile
 @endsection
